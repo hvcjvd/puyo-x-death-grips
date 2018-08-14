76ae012ab9df575d1b07f7a41035d2b6a06d6e46
TheHeroOfTrash <41023013+TheHeroOfTrash@users.noreply.github.com>
2018-08-14 00:34:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,35 +65,5 @@ client.on('message', message => {
   	}
 });
 
-```
-client.on('message', message => {
-    if (message.content === 'm!slots') {
-		var Slot1 = Math.floor(Math.random() * 5);   
-		var Slot2 = Math.floor(Math.random() * 5);
-		var Slot3 = Math.floor(Math.random() * 5);  
-		if (Slot1 === Slot2) {
-			if (Slot1 === Slot3) {
-				message.reply('3-Match Win! Super Lucky! \n', Slot1, Slot2, Slot3);
-				return;
-				}
-  		else {
-  			message.reply('2-Match Win! Good Job! \n', Slot1, Slot2, Slot3);
-			return;
-		}
-		if (Slot1 === Slot3) {
-			message.reply('2-Match Win! Good Job! \n', Slot1, Slot2, Slot3);
-			return;
-		}
-		if (Slot2 === Slot3) {
-			message.reply('2-Match Win! Good Job! \n', Slot1, Slot2, Slot3);
-			return;
-    }
-    if (Slot1 !== Slot2, Slot1 !== Slot2, Slot2 !== Slot3) {
-			message.reply('You lose fucker!', Slot1, Slot2, Slot3);
-			return;
-  	}
-```
-}}});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
