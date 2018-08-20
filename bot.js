62283d250029b0f5911f4cb5659cecd570b82453
TheHeroOfTrash <41023013+TheHeroOfTrash@users.noreply.github.com>
2018-08-20 18:46:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,5 +83,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'slottest') {
+    	message.reply('this shit is too hard just use miki');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
