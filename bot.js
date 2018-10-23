f2c0af9b003093fea0b6e0ff65e80702ef6e1e1a
TheHeroOfTrash <41023013+TheHeroOfTrash@users.noreply.github.com>
2018-10-22 22:38:31 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+'(remove this and apostorphes to reactivate bot) const Discord = require('discord.js');' 
 const client = new Discord.Client();
 
 client.on('ready', () => {
